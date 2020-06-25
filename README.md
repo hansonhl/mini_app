0f9131ae30bc37b845979ec42f09d27362c6a4a2
Hanson Lu <hansonlu.hl@gmail.com>
2020-06-25 18:36:43 -0500

    updates

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Web App for Wechat Mini Application
 
-This is a practice project
+This is a practice project from this imooc web development course 
+https://coding.imooc.com/learn/list/265.html
+
+All static files are provided by the instructor.
--- v0.1/routing.py
+++ v0.1/routing.py
@@ -2,6 +2,10 @@ from web.controllers.index import index_blueprint
 from web.controllers.imooc import route_imooc
 from web.controllers.user.user import user_blueprint
 from web.controllers.account.account import account_bluprint
+from web.controllers.member.member import member_blueprint
+from web.controllers.food.food import food_blueprint
+from web.controllers.finance.finance import finance_blueprint
+from web.controllers.stat.stat import stat_blueprint
 
 from web.interceptors.errorhandlers import *
 
@@ -9,3 +13,7 @@ app.register_blueprint(index_blueprint, url_prefix="/")
 app.register_blueprint(route_imooc, url_prefix="/imooc")
 app.register_blueprint(user_blueprint, url_prefix="/user")
 app.register_blueprint(account_bluprint, url_prefix="/account")
+app.register_blueprint(member_blueprint, url_prefix="/member")
+app.register_blueprint(food_blueprint, url_prefix="/food")
+app.register_blueprint(finance_blueprint, url_prefix="/finance")
+app.register_blueprint(stat_blueprint, url_prefix="/stat")
